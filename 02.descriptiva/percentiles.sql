41e9f70667d669be65f0f63ae323bf5e2ddca7e2
AndresVelasco <andresvelasco.me@gmail.com>
2024-12-01 18:08:49 +0100

    Update percentiles.sql

--- 02.descriptiva/percentiles.sql
+++ 02.descriptiva/percentiles.sql
@@ -3,7 +3,7 @@ SELECT
   ROUND(AVG(item.price),2) AS mean_revenue,
   ROUND(APPROX_QUANTILES(item.price,4)[OFFSET(1)],2) AS percent_25,
   ROUND(APPROX_QUANTILES(item.price,4)[OFFSET(2)],2) AS percent_50,
-  ROUND(APPROX_QUANTILES(item.price,4)[OFFSET(3)],2) AS percent_50,
+  ROUND(APPROX_QUANTILES(item.price,4)[OFFSET(3)],2) AS percent_75,
 FROM
   `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_2021*` t, UNNEST (items) item
 WHERE
